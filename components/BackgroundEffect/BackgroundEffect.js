664671882a3ac246217c3c455912168ea5dd5b8c
Dominic Clerici <dclerici77@gmail.com>
2023-06-20 02:42:57 -0700

    first backg effect

--- components/BackgroundEffect/BackgroundEffect.js
+++ components/BackgroundEffect/BackgroundEffect.js
@@ -1,12 +1,27 @@
 "use client"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import styles from "@/components/BackgroundEffect/BackgroundEffect.module.css"
 
+
 const BackgroundEffect = () => {
+const [counter, setCounter] = useState(0)
+  const makeParticle = () => {
+    let rect = document.getElementById('objectArea').getBoundingClientRect()
+    let x = Math.round(Math.random() * rect.right)
+    // let y = Math.random() * rect.bottom
+    // console.log(x,y)
+      let newEl = document.createElement('div')
+      newEl.style.left = x + 'px'
+      newEl.className = ['fallingObj']
+      document.getElementById("objectArea").appendChild(newEl)
+      setCounter(counter+1)
+  }
+  useEffect(() => {
+      setTimeout(makeParticle, 2000)
+  }, [makeParticle])
   return (
-    <div className={styles.effectBlurCont}>
-      <div id="mouseX" className={styles.object}></div>
-      <div className={styles.effect}></div>
+    <div className={styles.effectBlurCont} id="objectArea">
+]
     </div>
   )
 }
