a82278a76f603a9de7b54858273ee8c98330f9b1
dclerici <dclerici77@gmail.com>
2023-08-07 20:58:58 -0700

    startjs

--- components/Project/Project.js
+++ components/Project/Project.js
@@ -10,18 +10,18 @@ const Project = () => {
       <h2 className="defaultHeading">Projects</h2>
       <div className="hoverCont">
         <Module
+          link={"https://startjs.dominicclerici.com/"}
+          img={spo}
+          title="Start.js"
+          desc="A full stack web application made with Next.js and Prisma. Features cosmetic customization, a reminder system, user modifiable shortcuts, and a live suggestion Google search bar."
+          skills={["Next.js", "Prisma", "PostgreSQL", "Next-auth"]}
+        ></Module>
+        <Module
           link={"https://www.spotifystats.dominicclerici.com/"}
           img={start}
           title="Spotify Stats"
           desc="Displays your spotify listening stats. Shows your most listened to artists and songs over three timeframes."
           skills={["API", "User Authentication", "Next.js", "CSS Modules"]}
-        ></Module>
-        <Module
-          link={"https://www.startjs.dominicclerici.com/"}
-          img={spo}
-          title="Start.js"
-          desc="A simple start page made with Next.js. Features cosmetic customization, as well as user modifiable shortcuts. "
-          skills={["Next.js", "Localstorage", "Cookies", "Dynamic Styling"]}
         ></Module>
         <Module
           link={"https://github.com/DominicClerici/sorting-visualizer"}
