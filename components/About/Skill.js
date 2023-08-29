4b6c8aa8015c18c74db9e34e4de503f3f15ccae5
dclerici <dclerici77@gmail.com>
2023-08-29 18:37:26 -0400

    changed about section

--- components/About/Skill.js
+++ components/About/Skill.js
@@ -1,13 +1,13 @@
 import React from "react"
 import styles from "@/components/About/About.module.css"
 
-const Skill = ({ children, percent }) => {
+const Skill = ({ children }) => {
   return (
     <div className={styles.indivSkill}>
+      <svg className={styles.skillIcon} width={30} height={30}>
+        <polygon points="15,5 27,27 3,27" />
+      </svg>
       <p className={styles.skillLabel}>{children}</p>
-      <div className={styles.skillBack}>
-        <div className="skillBar" style={{ width: `${percent}%` }}></div>
-      </div>
     </div>
   )
 }
